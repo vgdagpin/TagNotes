15e0993f9221c02a49cac53977a233ed7c9d4b84
Vincent Dagpin <vincent.dagpin@gmail.com>
2025-09-02 03:09:12 +0800

    fix: update Docker run command to map port 3000 to 80 and add nginx configuration for SPA

--- Docker/run.cmd
+++ Docker/run.cmd
@@ -3,4 +3,4 @@ cd..
 docker build -t tagnotes -f Src/Dockerfile .
 docker stop tagnotes || true
 docker rm tagnotes || true
-docker run -d -p 3000:3000 --name tagnotes tagnotes
+docker run -d -p 3000:80 --name tagnotes tagnotes
