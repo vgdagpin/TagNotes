62d937e4acbc39ea5b0ac744cccc3da0f23a505b
vgdagpin <vincent.dagpin@gmail.com>
2025-09-02 19:38:30 +0800

    fix: update target port for ingress in container_app resource from 3000 to 80

--- Terraform/container_app.tf
+++ Terraform/container_app.tf
@@ -52,7 +52,7 @@ resource "azurerm_container_app" "app" {
 
   ingress {
     external_enabled = true
-    target_port      = 3000
+    target_port      = 80
     traffic_weight {
       latest_revision = true
       percentage      = 100
