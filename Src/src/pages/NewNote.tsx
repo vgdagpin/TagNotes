09e7eccf8943cb9e4963406e5ff6bbf842cdb5e6
Vincent Dagpin <vincent.dagpin@gmail.com>
2025-09-04 16:12:58 +0800

    feat: enhance NewNote component with note fetching and directory handling logic

--- Src/src/pages/NewNote.tsx
+++ Src/src/pages/NewNote.tsx
@@ -1,14 +1,68 @@
-import { useLocation } from "react-router-dom";
+import { getNote, getPersistedDirectoryHandle, isLocalMode } from "@/lib/notesClient";
+import { Note } from "@/shared/models";
+import { useEffect, useState } from "react";
 
 export default function NewNote() {
 
-    console.log('new note', useLocation());
+    const [isDirLoaded, setIsDirLoaded] = useState<boolean | undefined>(undefined);
+    const [noteTags, setNoteTags] = useState<string[]>([]);
 
-    
+    const [note, setNote] = useState<Note>({
+        id: '85f6330c-05aa-4beb-9841-0fac893f573f',
+        title: "",
+        tags: [],
+        sections: [],
+        createdAt: new Date(),
+        updatedAt: new Date(),
+    });
+
+    useEffect(() => {
+        let active = true;
+        const fetchNote = async () => {
+            try {
+                if (!isDirLoaded) return;
+                const data = await getNote('85f6330c-05aa-4beb-9841-0fac893f573f');
+                if (!active) return;
+                console.log('setNote from useEffect');
+
+                setNote(data);
+
+                setNoteTags(data.tags);
+            } catch { }
+        };
+        fetchNote();
+        return () => { active = false; };
+    }, [isDirLoaded]);
+
+    useEffect(() => {
+        let localMode = isLocalMode();
+
+        const tryGetPersistedDirectoryHandle = async () => {
+            const dir = await getPersistedDirectoryHandle();
+
+            setIsDirLoaded(!!dir);
+        }
+
+        if (!localMode) {
+            tryGetPersistedDirectoryHandle();
+        }
+
+        setIsDirLoaded(localMode);
+
+        // Listen for directory selection changes -> refresh list immediately
+        const onDirChanged = async () => {
+            setIsDirLoaded(true);
+        };
+
+        window.addEventListener('tagnotes:directoryChanged', onDirChanged as any);
+    }, []);
 
     return (
         <div className="h-screen bg-background flex">
-            <div className="flex-1 min-w-0"> Test
+            <div className="flex-1 min-w-0">
+                {!isDirLoaded && <p>Directory is not loaded</p>}
+                {isDirLoaded && (<><pre>{JSON.stringify(noteTags, null, 2)}</pre>
+                    <pre>{JSON.stringify(note, null, 2)}</pre></>)}
             </div>
         </div>
     );
